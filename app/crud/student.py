3e5d521d8e007dbd74d4feea98c6926a991aa5bb
Mauricio L <maurolanm@gmail.com>
2025-05-29 16:27:20 -0500

    crud student create completed

---
+++ app/crud/student.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from schemas.student import StudentCreation
+from models.student import Student
+
+def create_student(db: Session, student_data: StudentCreation):
+    new_student = Student(
+        name = student_data.name,
+        email = student_data.email
+    )
+    db.add(new_student)
+    db.commit()
+    db.refresh(new_student)
+
+    return new_student
