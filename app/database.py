7f05263565c4076d0c0e17293508ccc27359a9f6
Mauricio L <maurolanm@gmail.com>
2025-05-29 15:56:47 -0500

    database file completed with engine, session, base and get_db func

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./test.db'
 
@@ -11,3 +12,11 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
