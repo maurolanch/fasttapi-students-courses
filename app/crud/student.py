718f8af96d788e074bfb4fe6e95bd9e2b8c6b384
Mauricio L <maurolanm@gmail.com>
2025-05-30 11:11:14 -0500

    student crud update and delete func created also studentschema studentupdate added

--- app/crud/student.py
+++ app/crud/student.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from schemas.student import StudentCreation
+from schemas.student import StudentCreation, StudentUpdate
 from models.student import Student
 
 def create_student(db: Session, student_data: StudentCreation):
@@ -15,5 +15,28 @@ def create_student(db: Session, student_data: StudentCreation):
 
 def get_student_by_id(db: Session, student_id: int):
     return db.query(Student).filter(Student.id == student_id).first()
+
+def update_student_by_id(db: Session, student_id: int, student_update: StudentUpdate):
+    student = db.query(Student).filter(Student.id == student_id).first()
+    if not student:
+        return None
+    
+    update_data = student_update.model_dump(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(student, key, value)
+
+    db.commit()
+    db.refresh(student)
+    return student
+
+def delete_student_by_id(db: Session, student_id: int):
+    student = db.query(Student).filter(Student.id == student_id).first()
+    if not student:
+        return None
+    else:
+        db.delete(student)
+        db.commit()
+    return student
 
 
--- app/schemas/student.py
+++ app/schemas/student.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import Optional
 
 class StudentBase(BaseModel):
     name: str
@@ -14,6 +15,10 @@ class StudentOut(StudentBase):
 
     class Config:
         orm_mode = True
+
+class StudentUpdate(BaseModel):
+    name: Optional[str] = None
+    email: Optional[EmailStr] = None
 
 class StudentDelete(BaseModel):
     id: int
